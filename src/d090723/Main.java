399587ae43345ce5bf77619649b24e992e18de1d
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-07-09 16:37:19 +0530

    to handle for delimiters

--- src/d090723/Main.java
+++ src/d090723/Main.java
@@ -5,6 +5,15 @@ public class Main {
 	public int add(String numbers) {
 		if(numbers.isEmpty()) return 0;
 		if(numbers.length()<2) return Integer.parseInt(numbers);
+		else {
+			String delimiter = ",";
+			if(numbers.matches("//(.*)\n(.*)")) {
+				delimiter = Character.toString(numbers.charAt(2));
+				numbers = numbers.substring(4);
+			}
+			String[] list = divideNumbers(numbers, delimiter +"|\n");
+			return addition(list);
+		}
 		return 0;
 	}
 	public static void main(String[] args) {
