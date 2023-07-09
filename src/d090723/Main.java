6a5a6b120a15bbfc58d6cb256645fb3cdb2b53a7
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-07-09 16:17:51 +0530

    test for empty string

--- src/d090723/Main.java
+++ src/d090723/Main.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 public class Main {
 	@Test
 	int add(String numbers) {
-		
+		if(numbers.isEmpty()) return 0;
+		return 0;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
